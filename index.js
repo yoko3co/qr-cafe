439d5dc2cd50da08e097bc5c9992304ca27df101
Rafal Kuczowicz <rafal@Rafals-MacBook-Pro.local>
2026-04-30 21:30:01 +0200

    fix render port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const QRCode = require('qrcode');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // store sessions in memory
 const sessions = new Map();
