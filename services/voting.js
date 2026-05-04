bfd12a7fc42768e800b3aa015e256ad43a8c4224
QR Cafe System <no-reply@qr-cafe.local>
2026-05-04 19:41:41 +0200

    revert: back to working Krolestwo.3.1 before presentation

--- services/voting.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-let blockchainVoting = true;
-function getBlockchainVoting()      { return blockchainVoting; }
-function setBlockchainVoting(value) { blockchainVoting = value; }
-
-module.exports = { getBlockchainVoting, setBlockchainVoting };
